b99725225277cc096d3dd422894fba70af9ce336
veljkogligoric <58980309+veljkogligoric@users.noreply.github.com>
2019-12-26 09:42:30 +0100

    Add files via upload
    fire and gorila movement

---
+++ MyProject/Fire.py
@@ -0,0 +1,37 @@
+from PyQt5.QtCore import QThread,QObject,pyqtSignal,pyqtSlot
+from random import randint
+import time
+
+
+class FireMovement(QObject):
+    fireMovementSignal = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.thread = QThread()
+        # move the Worker object to the Thread object
+        # "push" self from the current thread to this thread
+        self.moveToThread(self.thread)
+        # Connect Thread started signal to Worker operational slot method
+        self.thread.started.connect(self.__work__)
+
+    def start(self):
+        """
+        Start notifications.
+        """
+        self.thread.start()
+
+    def die(self):
+        """
+        End notifications.
+        """
+        self.thread.quit()
+
+    @pyqtSlot()
+    def __work__(self):
+        """
+        A slot with no params.
+        """
+        while True:
+            self.fireMovementSignal.emit()
+            time.sleep(0.08)
