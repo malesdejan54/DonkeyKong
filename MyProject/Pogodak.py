0794babf9a2d03106588dd0e697eb551b1f55da6
Joca998 <58732732+Joca998@users.noreply.github.com>
2020-01-16 14:33:27 +0100

    Add files via upload

---
+++ MyProject/Pogodak.py
@@ -0,0 +1,40 @@
+import time
+from random import randint
+
+def isHit(first, second):
+    rec1 = first.geometry()
+    y1 = first.height()
+    x1 = first.width()
+
+    rec2 = second.geometry()
+    x2 = second.width()
+    y2 = second.height()
+
+    if rec1.x() in range(rec2.x(), rec2.x() + x2):
+        if rec1.y() in range(rec2.y(), rec2.y() + y2):
+            return True
+        elif rec1.y() + y1 in range(rec2.y(), rec2.y() + y2):
+            return True
+
+    if rec1.x() + x1 in range(rec2.x(), rec2.x() + x2):
+        if rec1.y() in range(rec2.y(), rec2.y() + y2):
+            return True
+        elif rec1.y() + y1 in range(rec2.y(), rec2.y() + y2):
+            return True
+
+def GenerateBarrel(q):
+    while True:
+       #q.put(1)
+       time.sleep((randint(8, 12)))
+
+def GorilaFreeze(start, stop):
+    while True:
+       # a = stop.get()
+        time.sleep(5)
+        start.put(1)
+
+def restartPlayer(label, p):
+    if p == 1:
+        label.setGeometry(250, 460, 36, 50)
+    else:
+        label.setGeometry(730, 436, 75, 100)
